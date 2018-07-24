da007dddf6950245f5cb68195a20b256ed9a95d0
meilihao <563278383@qq.com>
2018-07-24 13:58:35 +0800

    + 字典来源: 汉典

--- doc.go
+++ doc.go
@@ -2,4 +2,5 @@
 // [Unicode Range "CJK Unified Ideographs"](http://www.unicodemap.org/range/71/CJK_Unified_Ideographs/): 0x4E00~0x9FEA
 // source:
 // - [xh.5156edu.com/](http://xh.5156edu.com/pinyi.html)
+// - [汉典 ](http://www.zdic.net/z/pyjs/)
 package pinyin
